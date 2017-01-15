2db739427c25123a2f83234bfb56b31c2bc070a1
baiko <baikou.yauheni@gmail.com>
2017-01-15 12:05:21 +0200

    Initial commit

---
+++ Kotitehtävät_1_2/T1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+/// <summary>
+/// Tehtävä 1
+///Tee ohjelma, joka tulostaa käyttäjän antamaa lukua(1, 2 tai 3) vastaavan luvun sanana(yksi, kaksi tai kolme).
+/// Jos käyttäjä syöttää jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku".
+/// </summary>
+
+
+namespace T1
+{
+    class T1
+    {
+        static void Main(string[] args)
+        {
+
+            int luku;
+            System.Console.WriteLine("Anna luku > ");
+
+            luku = Convert.ToInt32(Console.ReadLine());
+
+            if (luku == 1 || luku == 2 || luku == 3)
+            {
+                if (luku == 1)
+                    System.Console.WriteLine("Annoit luvun yksi ");
+                if (luku == 2)
+                    System.Console.WriteLine("Annoit luvun kaksi ");
+                if (luku == 3)
+                    System.Console.WriteLine("Annoit luvun kolme ");
+            }
+            else
+                System.Console.WriteLine("joku muu luku ");
+        }
+    }
+}
