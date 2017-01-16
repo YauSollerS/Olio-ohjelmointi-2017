42bfb5c39dc8c84464599a2c5596b1e7865a9d00
K8792 <K8792@student.jamk.fi>
2017-01-16 10:49:43 +0200

    Initial commit

---
+++ Kotitehtävät_1_2/T10/T10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T10
+{
+    class T10
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[9] { 1,2,33,44,55,68,77,96,100};
+
+            for (int i=0; i<arr1.Length;i++)
+            {
+                int jak = arr1[i] % 2;
+                if (jak == 0)
+                    System.Console.Write(arr1[i] + " HEP! \n");
+                else
+                    System.Console.Write(arr1[i] + "\n");
+            }
+
+        }
+    }
+}
