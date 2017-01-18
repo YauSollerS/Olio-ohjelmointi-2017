54a74a3475bb258e912691ed0b4632f3c94a0ef2
K8792 <baikou.yauheni@gmail.com>
2017-01-18 15:44:04 +0200

    Initial Commit

---
+++ TunninTehtävät/Labra04/Hissi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra04.T1
+
+{
+    class Hissi
+    {
+        private const int minFloor = 1;
+        private const int maxFloor = 5;
+        private int floor = 1;
+        private bool isFloorValid;
+        
+        public int Floor
+        {
+            get { return floor; }
+            set {
+                
+                if (value<minFloor)
+                {
+                    isFloorValid = false;
+                }
+                else if (value>maxFloor)
+                {
+                    isFloorValid = false;
+                }
+                else
+                {
+                    floor = value;
+                    isFloorValid = true;
+                }
+            }
+        }
+        
+
+        public bool IsFloorValid
+        {
+            get { return IsFloorValid; }
+        }
+
+        public string FloorMessage
+        {
+            get
+            {
+                if (IsFloorValid)
+                {
+                    return "Valittu kerros " + floor;
+                }
+                else
+                    return "Valittu kerros ei ole käytettävissä";
+            }
+        }
+        
+    }
+}
---
+++ TunninTehtävät/Labra04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra04.T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hissi Elevator = new Hissi();
+            
+            
+
+            int lenght = 1;
+            for (int i = 0; i < lenght; i++)
+            {
+
+                Console.Write("Give a new floor number(1-5) > ");
+                string line = Console.ReadLine();
+                int number;
+                bool result = int.TryParse(line, out number);
+                Elevator.Floor = number;
+                Console.WriteLine( );
+                lenght++;
+                
+            }
+            
+        }
+    }
+}
