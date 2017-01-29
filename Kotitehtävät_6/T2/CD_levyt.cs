6c0ae09c0a19930236a65db44c129815c116f609
baiko <baikou.yauheni@gmail.com>
2017-01-29 12:17:50 +0200

    Initial Commit

---
+++ Kotitehtävät_6/T2/CD_levyt.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Song
+    {
+        public string Name { get; set; }
+        public string Lenght { get; set; }
+        
+        public Song (string nimi, string piitus)
+        {
+            Name = nimi;
+            Lenght = piitus;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Lenght ;
+        }
+    }
+
+    class CD
+    {
+        public string Name { get; set; }
+        public string Artist { get; set; }
+
+        private List<Song> songs;
+
+        public List<Song> SongsList
+        {
+            get { return songs; }
+        }
+        public CD( string nimi, string laulaja)
+        {
+            Name = nimi;
+            Artist = laulaja;
+            songs = new List<Song>();
+        }
+
+        public void AddSong(Song track)
+        {
+            songs.Add(track);
+        }
+
+        public override string ToString()
+        {
+            return "CD data:\n-name: " + Name + "\n-artist: " + Artist;
+        }
+    }
+}
---
+++ Kotitehtävät_6/T2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CD mutter = new CD("Mutter", "Rammstein");
+
+            Song so1 = new Song("Links 2-3-4", "3:36");
+            Song so2 = new Song("Sonne", "4:32");
+            Song so3 = new Song("Ich Will", "3:37");
+            Song so4 = new Song("Mutter", "4:28");
+
+
+            mutter.AddSong(so1);
+            mutter.AddSong(so2);
+            mutter.AddSong(so3);
+            mutter.AddSong(so4);
+
+            Console.WriteLine(mutter.ToString());
+
+            Console.WriteLine("-songs:");
+
+
+            foreach(Song s in mutter.SongsList)
+            {
+                Console.WriteLine(" - {0}", s.ToString());
+            }
+
+        }
+    }
+}
