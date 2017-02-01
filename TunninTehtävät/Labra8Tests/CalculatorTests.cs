473ee1142c95b982e05b6802f5079c00557803bf
K8792 <baikou.yauheni@gmail.com>
2017-02-01 14:09:25 +0200

    Initial Commit

---
+++ TunninTehtävät/Labra8Tests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labra8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra8.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ TunninTehtävät/Labra8Tests1/CalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labra8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra8.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            ///AAA periaate
+            ///A=Arrange, tietojen alustus
+            ///A=Act, kutsutaan metodia
+            ///A=Assert, varmistetaan ett tulos ok
+            //arrange
+            Calculator calc = new Calculator();
+            int a = 4;
+            int b = 5;
+            int expected = 9;
+            //act
+            int actual = calc.Add(a, b);
+            //assert
+            Assert.AreEqual(expected, actual);
+            //ja toinen testi Add-metodilla
+            a = 0;
+            b = 1;
+            expected = 1;
+            actual = calc.Add(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+
+            //arrange
+            Calculator calc = new Calculator();
+            int a = 4;
+            int b = 5;
+            int expected = 20;
+            //act
+            int actual = calc.Multiply(a, b);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
