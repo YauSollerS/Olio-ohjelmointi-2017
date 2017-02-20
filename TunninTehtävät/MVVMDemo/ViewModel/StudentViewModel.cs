77c320ae29ef1817db7da7814f55e1d9edf86387
K8792 <baikou.yauheni@gmail.com>
2017-02-20 13:01:07 +0200

    Initial Commit

---
+++ TunninTehtävät/MVVMDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMDemo.Model;
+using System.Collections.ObjectModel;
+
+namespace MVVMDemo.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Model.Student>();
+            //lisätään esimerkin vuoksi muutama opiskelija
+            students.Add(new Student { FirstName = "Kalle", LastName = "Jalkanen", Asioid="A1234" });
+            students.Add(new Student { FirstName = "Teppo", LastName = "Testaaja", Asioid = "B1234" });
+            students.Add(new Student { FirstName = "Tomi", LastName = "Töttenström", Asioid = "C1234" });
+            students.Add(new Student { FirstName = "Anni", LastName = "Ainokainen", Asioid = "D1234" });
+            Students = students;
+        }
+    }
+}
