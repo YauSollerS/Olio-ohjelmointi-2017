cf38092cde43467564bb4a8862d4576150b28ab4
K8792 <baikou.yauheni@gmail.com>
2017-03-22 12:50:32 +0200

    Commit

---
+++ TunninTehtävät/SnakeWPF/Game.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnakeWPF
+{
+    /// <summary>
+    /// Interaction logic for Game.xaml
+    /// </summary>
+    /// 
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+    public partial class Game : Window
+    {
+        //variables and consts
+        private const int minimi= 5;
+        private const int maxHeight = 620;
+        private const int maxWidth = 380;
+        private int snakeWidth = 10;
+        private int snakeLength = 100;
+        private int easiness = 20;
+        private int score = 0;
+
+        public Game()
+        {
+            InitializeComponent();
+        }
+    }
+}
