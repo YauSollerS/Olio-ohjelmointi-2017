9a6504f800af0eea7638f5788bc750b7ae8c57bb
baiko <baikou.yauheni@gmail.com>
2017-03-26 20:52:51 +0300

    commit

---
+++ HT/HT/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using HT.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace HT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _clockTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _clockTimer = new DispatcherTimer();
+            _clockTimer.Interval = new TimeSpan(1000);
+            _clockTimer.Tick += _clockTimer_Tick;
+            _clockTimer.Start();
+
+
+
+        }
+
+        private void _clockTimer_Tick(object sender, EventArgs e)
+        {
+            clockLabel.Content = DateTime.Now.ToShortTimeString();
+        }
+
+        private void btnTuotteet_Click(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = new TuotteetViewModel();
+        }
+
+        private void btnMyynti_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
